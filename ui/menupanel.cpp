1e54ff194135bd3fbf3f7d0f87d574c68a39cdc1
Dmitry Zykov <dmitriizykov64@gmail.com>
2017-03-26 20:51:17 +0300

    Fixed menu

--- ui/menupanel.cpp
+++ ui/menupanel.cpp
@@ -4,42 +4,42 @@
 void MenuPanel::closeCurrent()
 {
     switch(_state) {
-        case MenuState::HelpMenuState:
+    case MenuState::HelpMenuState:
         _state = MenuState::DefaultState;
         ui->helpButton->setPalette(_defaultPalette);
         emit helpMenuClosed();
         break;
-        case MenuState::ModeMenuState:
+    case MenuState::ModeMenuState:
         _state = MenuState::DefaultState;
         ui->modeButton->setPalette(_defaultPalette);
         emit modeMenuClosed();
         break;
-        case MenuState::CalibrationsMenuState:
+    case MenuState::CalibrationsMenuState:
         _state = MenuState::DefaultState;
         ui->calibrationButton->setPalette(_defaultPalette);
         emit calibrationsMenuClosed();
         break;
-        case MenuState::RegistrationMenuState:
+    case MenuState::RegistrationMenuState:
         _state = MenuState::DefaultState;
         ui->registrationButton->setPalette(_defaultPalette);
         emit registrationMenuClosed();
         break;
-        case MenuState::MemoryMenuState:
+    case MenuState::MemoryMenuState:
         _state = MenuState::DefaultState;
         ui->memoryButton->setPalette(_defaultPalette);
         emit memoryMenuClosed();
         break;
-        case MenuState::OptionsMenuState:
+    case MenuState::OptionsMenuState:
         _state = MenuState::DefaultState;
         ui->optionsButton->setPalette(_defaultPalette);
         emit optionsMenuClosed();
         break;
-        case MenuState::SystemMenuState:
+    case MenuState::SystemMenuState:
         _state = MenuState::DefaultState;
         ui->systemButton->setPalette(_defaultPalette);
         emit systemMenuClosed();
         break;
-        case MenuState::DefaultState:
+    case MenuState::DefaultState:
         return;
         break;
     }
@@ -67,77 +67,91 @@ void MenuPanel::resetMenu()
 
 void MenuPanel::on_helpButton_released()
 {
-    if(_state == DefaultState) {
+    if(_state == MenuState::HelpMenuState) {
+        closeCurrent();
+    }
+    else {
+        closeCurrent();
         _state = MenuState::HelpMenuState;
         ui->helpButton->setPalette(_activePalette);
         emit helpMenuOpened();
-    } else {
-        closeCurrent();
     }
 }
 
 void MenuPanel::on_modeButton_released()
 {
-    if(_state == DefaultState) {
+    if(_state == MenuState::ModeMenuState) {
+        closeCurrent();
+    }
+    else {
+        closeCurrent();
         _state = MenuState::ModeMenuState;
         ui->modeButton->setPalette(_activePalette);
         emit modeMenuOpened();
-    } else {
-        closeCurrent();
     }
 }
 
 void MenuPanel::on_registrationButton_released()
 {
-    if(_state == DefaultState) {
+    if(_state == MenuState::RegistrationMenuState) {
+        closeCurrent();
+    }
+    else {
+        closeCurrent();
         _state = MenuState::RegistrationMenuState;
         ui->registrationButton->setPalette(_activePalette);
         emit registrationMenuOpened();
-    } else {
-        closeCurrent();
     }
 }
 
 void MenuPanel::on_memoryButton_released()
 {
-    if(_state == DefaultState) {
+    if(_state == MenuState::MemoryMenuState) {
+        closeCurrent();
+    }
+    else {
+        closeCurrent();
         _state = MenuState::MemoryMenuState;
         ui->memoryButton->setPalette(_activePalette);
         emit memoryMenuOpened();
-    } else {
-        closeCurrent();
     }
 }
 
 void MenuPanel::on_optionsButton_released()
 {
-    if(_state == DefaultState) {
+    if(_state == MenuState::OptionsMenuState) {
+        closeCurrent();
+    }
+    else {
+        closeCurrent();
         _state = MenuState::OptionsMenuState;
         ui->optionsButton->setPalette(_activePalette);
         emit optionsMenuOpened();
-    } else {
-        closeCurrent();
     }
 }
 
 void MenuPanel::on_systemButton_released()
 {
-    if(_state == DefaultState) {
+    if(_state == MenuState::SystemMenuState) {
+        closeCurrent();
+    }
+    else {
+        closeCurrent();
         _state = MenuState::SystemMenuState;
         ui->systemButton->setPalette(_activePalette);
         emit systemMenuOpened();
-    } else {
-        closeCurrent();
     }
 }
 
 void MenuPanel::on_calibrationButton_released()
 {
-    if(_state == DefaultState) {
+    if(_state == MenuState::CalibrationsMenuState) {
+        closeCurrent();
+    }
+    else {
+        closeCurrent();
         _state = MenuState::CalibrationsMenuState;
         ui->calibrationButton->setPalette(_activePalette);
         emit calibrationsMenuOpened();
-    } else {
-        closeCurrent();
     }
 }
